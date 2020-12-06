10d1645df2432ed5efb2dd497e1740854aaa3aef
yuukii125 <y.kipro125@gmail.com>
2020-12-06 22:26:59 +0900

    サロンの一意性

--- app/models/salon.rb
+++ app/models/salon.rb
@@ -1,6 +1,6 @@
 class Salon < ApplicationRecord
-  validates :name, presence: true
-  validates :address, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :address, presence: true, uniqueness: true
 
   belongs_to :user
   has_many :reviews, dependent: :destroy  
